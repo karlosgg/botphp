678970d01ff80bdbed8ad6a67c21630702c7c627
jcgrijalva <josecarlosgrijalva@gmail.com>
2017-03-24 14:50:40 -0600

    connecting

--- web/views/monitor.php
+++ web/views/monitor.php
@@ -31,7 +31,7 @@
 	  $text = "El cliente no esta registrado.\n El cliente ".$nombre." se ha registrado satisfactoriamente.";
 
 	}else{
-		$text="El cliente ya esta registrado. ".pg_query($db, "SELECT count(*) FROM Clientes WHERE Chat='".$chatId."'");
+		$text="El cliente ya esta registrado. ".$chatId;
 	}
 	pg_close($db);
 
