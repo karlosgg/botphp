1c28f572afafa56a8a42edc3154abe1ac8255437
jcgrijalva <josecarlosgrijalva@gmail.com>
2017-03-24 14:31:43 -0600

    connecting

--- web/data/connect.php
+++ web/data/connect.php
@@ -6,6 +6,21 @@
    } else {
       echo "Opened database successfully\n";
    }
+
+    $sql ="SELECT * FROM Clientes";
+
+   $ret = pg_query($db, $sql);
+   if(!$ret){
+      echo pg_last_error($db);
+      exit;
+   } 
+   while($row = pg_fetch_row($ret)){
+      echo "ID = ". $row[0] . "\n";
+      echo "Nombre = ". $row[1] ."\n";
+      echo "Chat = ". $row[2] ."\n";
+   }
+   echo "Operation done successfully\n";
+   pg_close($db);
    //$ret=pg_query($db,"TRUNCATE TABLE Clientes");
 
    /*$sql =<<<EOF
