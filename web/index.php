2a4d60f05ceda08f60dd3add5bd07df3c8dfac60
jcgrijalva <josecarlosgrijalva@gmail.com>
2017-03-24 10:43:05 -0600

    php-bot

--- web/index.php
+++ web/index.php
@@ -26,7 +26,7 @@ $app->get('/', function() use($app) {
 
 $app->get('/bot', function() use($app) {
   $app['monolog']->addDebug('logging output.');
-  return 'monitor.php';
+  return (include('monitor.php'));
 });
 
 $app->run();
