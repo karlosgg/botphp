25ba10c75bae946db261f4d4cb930293dec39a76
jcgrijalva <josecarlosgrijalva@gmail.com>
2017-03-24 13:45:30 -0600

    connecting

--- web/views/monitor.php
+++ web/views/monitor.php
@@ -25,14 +25,15 @@
 	
 	$result = pg_query($db, "SELECT * FROM Clientes WHERE Chat='".$chatId."'");
 	if (!$result) {
-	
+		
 		/*while ($row = pg_fetch_row($result)) {
 			  $text.= "IdCliente: $row[0]  Nomre: $row[1]";
 			}*/
-	  $text = $result;
+	  $text = "El cliente no esta registrado.";
 
 	}else{
-		$text = "El cliente ya esta registrado.";
+		$text=$result;
+		//$text = "El cliente ya esta registrado.";
 	}
 	pg_close($db);
 
