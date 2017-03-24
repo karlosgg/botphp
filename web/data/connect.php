f6581133ede143b6b65d7a7e252a658bf4f12119
jcgrijalva <josecarlosgrijalva@gmail.com>
2017-03-24 13:11:30 -0600

    connecting

--- web/data/connect.php
+++ web/data/connect.php
@@ -1,8 +1,4 @@
 <?php
-   $host        = "host=ec2-54-163-233-89.compute-1.amazonaws.com";
-   $port        = "port=5432";
-   $dbname      = "d93o9hfi386ju7";
-   $credentials = "user=aahmvpcqkyrxhi password=2f787330230735cc61d02129305fb98cbb54107ceb2894ba9956c8789d381624";
 
    $db = pg_connect( "postgres://aahmvpcqkyrxhi:2f787330230735cc61d02129305fb98cbb54107ceb2894ba9956c8789d381624@ec2-54-163-233-89.compute-1.amazonaws.com:5432/d93o9hfi386ju7"  );
    if(!$db){
@@ -10,4 +6,20 @@
    } else {
       echo "Opened database successfully\n";
    }
+
+   $sql =<<<EOF
+   	CREATE TABLE Clientes (
+    IdCliente   serial CONSTRAINT firstkey PRIMARY KEY,
+    Nombre      varchar(200),
+    Chat        varchar(100)
+	);
+EOF;
+
+	$ret = pg_query($db, $sql);
+	if(!$ret){
+      echo pg_last_error($db);
+   } else {
+      echo "Table created successfully\n";
+   }
+   pg_close($db);
 ?>
