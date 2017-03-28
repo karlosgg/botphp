32375d5a82ed5ac1bd972a4b1c694f1d44fd3641
Jose Carlos <karlosgg@gmail.com>
2017-03-27 23:48:29 -0600

    lala

--- web/views/verClientes.php
+++ web/views/verClientes.php
@@ -5,6 +5,21 @@ require('../data/connect.php');
 	  $text = "Error al conectar.";
 	  exit;
 	}
+
+	$result = pg_query($db, "SELECT count(*) as nom FROM Clientes ");
+	if ($result) {
+		
+		//pg_query($db, "INSERT INTO Clientes (Nombre, Chat) VALUES ('".$nombre."','".$chatId."')");
+		$res=0;
+		while($row=pg_fetch_assoc($result)){
+			$res=  $row['nom'];
+		}
+
+			 echo $res. " clientes registrados.";
+
+	 
+
+	}
 	
 	$result = pg_query($db, "SELECT * FROM Clientes");
 	if ($result) {
