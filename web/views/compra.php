8a6c11d72c6787951fd9f65eb3014b81e3c691ea
Jose Carlos <karlosgg@gmail.com>
2017-03-28 00:17:04 -0600

    lala

--- web/views/compra.php
+++ web/views/compra.php
@@ -5,10 +5,10 @@ $url = "https://api.telegram.org/bot" . $botToken;
 
 ?>
 <html>
-<head><title>Realizar Facturacion</title></head>
+<head><title>Enviar Archivo</title></head>
 <body>
 	<form action="<?php echo $url.'/sendPhoto' ?>" method="post" enctype="multipart/form-data">
-		<input type="text" name="chat_id" value="350172762" />
+		<input type="text" name="chat_id" value="<?php echo $GET['id']?>" />
 		<input type="file" name="photo" />
 		<input type="submit" value="send" />
 	</form>
