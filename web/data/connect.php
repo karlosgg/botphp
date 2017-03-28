f490aa7f2842650416fefbfaab7a26778de886af
Jose Carlos <karlosgg@gmail.com>
2017-03-28 00:28:10 -0600

    lala

--- web/data/connect.php
+++ web/data/connect.php
@@ -2,9 +2,9 @@
 
    $db = pg_connect( "postgres://aahmvpcqkyrxhi:2f787330230735cc61d02129305fb98cbb54107ceb2894ba9956c8789d381624@ec2-54-163-233-89.compute-1.amazonaws.com:5432/d93o9hfi386ju7"  );
    if(!$db){
-      //echo "Error : Unable to open database\n";
+      echo "Error : Unable to open database\n";
    } else {
-      echo "Opened database successfully\n";
+      //echo "Opened database successfully\n";
    }
 
    
