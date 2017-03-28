669dafaef4dfb98eaa6fd8222a55ffd53988d4d2
Jose Carlos <karlosgg@gmail.com>
2017-03-27 22:31:59 -0600

    lala

--- web/views/monitor.php
+++ web/views/monitor.php
@@ -24,7 +24,7 @@
 	  exit;
 	}
 	
-	$result = pg_query($db, "SELECT count(*) as num FROM Clientes WHERE Chat=".$chatId."");
+	$result = pg_query($db, "SELECT count(*) as num FROM Clientes WHERE Chat='".$chatId."'");
 	if (!$result) {
 		
 	//pg_query($db, "INSERT INTO Clientes (Nombre, Chat) VALUES ('".$nombre."','".$chatId."')");
